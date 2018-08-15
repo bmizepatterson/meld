7c43e693314cffe3044b4d31cf99dbdbda7dc99b
Ben Patterson <bmizepatterson@gmail.com>
2018-08-15 10:20:32 -0400

    Setup canvas, set window resize event listener

--- meld.js
+++ meld.js
@@ -1,14 +1,28 @@
+var canvas = document.getElementsByTagName('canvas')[0],
+	ctx = canvas.getContext('2d');
+
 // Utilities
 function clamp(n, min, max) {
   return Math.min(max, Math.max(min, n));
 }
 
 function draw() {
-
+	ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 
 function init() {
+	canvas.height = 400;
+	window.addEventListener('resize', resize);
+	resize();
+}
 
+function resize() {
+	canvas.width = window.innerWidth;
+	if (window.innerHeight > canvas.height) {
+		canvas.style.top = (window.innerHeight - canvas.height)/2+'px';
+	} else {
+		canvas.style.top = 0;
+	}
 }
 
 window.requestAnimFrame = (function(){
